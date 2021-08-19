005e6d1f33838afa56db41ce2ce697f65550fc6d
romulofgouvea <romulofgouvea@gmail.com>
2021-08-19 10:51:20 -0300

    Ajustando o console teste para uma pasta

---
+++ console/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.IO;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using HtmlToOpenXml;
+
+namespace DotnetHtmlToDocx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string filename = "test.docx";
+            string html = @"
+                <!DOCTYPE HTML PUBLIC
+                <html>
+                    <head>
+                        <title></title>
+                    </head>
+                    <body>
+                        Looks how cool is <font size='x-large'><b>Open Xml</b></font>.
+                        Now with <font color='red'><u>HtmlToOpenXml</u></font>, it nevers been so easy to convert html.
+                        <p>
+                            If you like it, add me a rating on <a href='https://github.com/onizet/html2openxml'>github</a>
+                        </p>
+                        <hr>
+                    </body>
+                </html>
+            ";
+
+            if (File.Exists(filename)) File.Delete(filename);
+
+            using (MemoryStream generatedDocument = new MemoryStream())
+            {
+                using (WordprocessingDocument package = WordprocessingDocument.Create(generatedDocument, WordprocessingDocumentType.Document))
+                {
+                    MainDocumentPart mainPart = package.MainDocumentPart;
+                    if (mainPart == null)
+                    {
+                        mainPart = package.AddMainDocumentPart();
+                        new Document(new Body()).Save(mainPart);
+                    }
+
+                    HtmlConverter converter = new HtmlConverter(mainPart);
+                    converter.ParseHtml(html);
+
+                    mainPart.Document.Save();
+                }
+
+                File.WriteAllBytes(filename, generatedDocument.ToArray());
+            }
+        }
+
+
+    }
+}
